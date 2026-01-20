89b651158877831185c263b47e14a5701d362b07
Morgan Powers <morgan.c.powers@gmail.com>
2026-01-20 14:26:24 -0600

    pruned palindromeNumber

--- palindromeNumber.r
+++ palindromeNumber.r
@@ -22,5 +22,5 @@ total  <- 4
 passed <- passed + ifelse(isPalindrome(1),1,0)
 passed <- passed + ifelse(isPalindrome(8008),1,0)
 passed <- passed + ifelse(isPalindrome(12321),1,0)
-passed <- passed + ifelse(isPalindrome((880226615529888473330265269768646444333433887733883465996765424854458424567699564388337788334333444646867962562033374888925516622088)),1,0)
+passed <- passed + ifelse(isPalindrome(880226615529888473330265269768646444333433887733883465996765424854458424567699564388337788334333444646867962562033374888925516622088),1,0)
 cat(ifelse(passed==total,green("Test passed!\n"),red(paste0("Test failed with ",passed,"/",total,"\n"))))
