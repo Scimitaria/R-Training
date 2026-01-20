db57291e477d390b8e53086cab734ffb0adb4481
Morgan Powers <morgan.c.powers@gmail.com>
2026-01-20 13:33:30 -0600

    added findRoot

---
+++ findRoot.r
@@ -0,0 +1,28 @@
+if(!require(crayon))install.packages("crayon")
+library(crayon)
+
+# Square Root #
+# Given a non-negative number, return the square root rounded down to the nearest integer
+# eg given 17, the answer is 4
+# You ARE allowed to use the exponent operator ^ (eg 2^3 = 8)
+# You are NOT allowed to use sqrt()
+
+# HINT:
+# you can construct an range of numbers with :
+# eg 1:3 is the same as c(1,2,3)
+
+findRoot <- function(num){
+    #your code here
+    return(-1)
+}
+
+# Test code #
+# no touchy
+passed <- 0
+total  <- 5
+passed <- passed + ifelse(findRoot(0)==0,1,0)
+passed <- passed + ifelse(findRoot(1)==1,1,0)
+passed <- passed + ifelse(findRoot(16)==4,1,0)
+passed <- passed + ifelse(findRoot(26)==5,1,0)
+passed <- passed + ifelse(findRoot(4819136400)==69,1,0)
+cat(ifelse(passed==total,green("Test passed!\n"),red(paste0("Test failed with ",passed,"/",total,"\n"))))
